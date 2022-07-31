f86b01ffa85a7d2ccb804d805949285a3071ba0a
Alkarias <alkarias29@gmail.com>
2022-07-31 15:38:01 -0400

    added basic html structure

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inquirer = require('inquirer');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
+const { resolvePtr } = require('dns');
 //variables
 const fileName = 'index.html';
 const employeeList = [];
@@ -32,7 +33,7 @@ const managerQuestions = [
     },
     {
         type: 'list',
-        message: 'Would you like to add an Engineer or Intern?',
+        message: 'Would you like to add another employee?',
         choices: ['yes','no'],
         name: 'addEmployee'
     }
@@ -61,7 +62,7 @@ const engineerQuestions = [
     },
     {
         type: 'list',
-        message: 'Would you like to add an Engineer or Intern?',
+        message: 'Would you like to add another employee?',
         choices: ['yes','no'],
         name: 'addEmployee'
     }
@@ -90,30 +91,18 @@ const internQuestions = [
     },
     {
         type: 'list',
-        message: 'Would you like to add an Engineer or Intern?',
+        message: 'Would you like to add another employee?',
         choices: ['yes','no'],
         name: 'addEmployee'
     }
 ]
-
-function writeManager(res) {
-    return ``;
-}
-
-function writeEngineer(res) {
-    return ``;
-}
-
-function writeIntern(res) {
-    return ``;
-}
 
 function pickType() {
     // ask which type of employee should be added
     inquirer.prompt(
         {
             type: 'list',
-            message: "Would you like to add another employee?",
+            message: "Are they an engineer or intern?",
             choices: ['Engineer', 'Intern'],
             name: 'role'
         }
@@ -154,10 +143,64 @@ function populateIntern() {
 }
 
 function writeToFile() {
-    let data = "";
-    //this line is temporary for checking results
-    employeeList.forEach(employee => data += `${employee.getRole()}:${employee.getName()}\n`);
+    let data = generateHtml();
     fs.writeFileSync(`./dist/${fileName}`, data);
+}
+
+function generateHtml() {
+    //start the file with the necessary setup and metadata
+    let content = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Team Generator</title>
+</head>
+<body class="container-fluid p-0">
+    <header class="bg-danger text-light py-4 mb-4">
+        <h1 class="text-center">My Team</h1>
+    </header>
+
+    <section class="d-flex justify-content-center">`;
+//----------------------------------
+// This is where the logic to generate the cards goes
+    content += writeEmployee(employeeList[0]);
+    // writing this as individual loops ensures that the roles will be grouped together
+    employeeList.forEach(employee => {
+        if (employee.getRole() === 'Engineer') content += writeEmployee(employee);
+    }); 
+    employeeList.forEach(employee => {
+        if (employee.getRole() === 'Intern') content += writeEmployee(employee);
+    })  
+//----------------------------------
+
+// closing open element tags
+content += `
+    </section>
+</body>
+</html>`;
+
+    return content;
+}
+
+function writeEmployee(employee) {
+    return `
+        <div class="shadow m-3">
+            <div class="bg-primary text-light p-2">
+                <h3>${employeeList[0].getName()}</h3>
+                <h4>${employeeList[0].getRole()}</h4>
+            </div>
+            <ul class="list-group p-3">
+                <li class="list-group-item">ID: ${employeeList[0].getId()}</li>
+                <li class="list-group-item">
+                    <a href="mailto:${employeeList[0].getEmail()}">${employeeList[0].getEmail()}</a>
+                </li>
+                <li class="list-group-item">${}</li>
+            </ul>
+        </div>`;
 }
 
 function init() {
